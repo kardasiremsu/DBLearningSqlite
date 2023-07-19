84277372f7db47501d96401b1bfba74c8394a877
kardasiremsu <kardasiremsu@gmail.com>
2023-07-19 20:23:12 +0300

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
